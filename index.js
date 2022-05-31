617bded280293ec9f1f0c871945e7b234ec9d07c
aliceauko <alice.auko@student.moringaschool.com>
2022-05-31 22:02:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  let cards= []
+  for (let i = 0; i < names.length; i++) {
+    cards.push("Thank you, " + names[i] + ", for the wonderful " + event + " gift!")
+  }
+  return cards;
+}
+
+
+
+function countDown(currentNumber) {
+  console.log(currentNumber);
+
+  let toNumber = currentNumber - 1;
+
+  if (toNumber >=0) {
+      countDown(toNumber);
+  }
+}
+countDown(10);
+
